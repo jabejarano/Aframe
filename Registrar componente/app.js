93a2d9d2ba29fcda0a01fa3aba3da195f706a4db
José Antonio Bejarano Sigüenza <96siguenza@gmail.com>
2023-11-24 13:01:04 +0100

    Cambio para dañar ovni con click

--- Registrar componente/app.js
+++ Registrar componente/app.js
@@ -34,29 +34,40 @@ AFRAME.registerComponent('log2', {
   });
 
 
-  AFRAME.registerComponent('log4', {
+  AFRAME.registerComponent('log', {
     schema: {
       event: {type: 'string', default: ''},
       message: {type: 'string', default: 'Hello, World!3'}
     },
-
+  
+    // Este es el lugar donde inicializamos nuestro componente cuando se adjunta.
     init: function () {
-        // Este es el lugar para la lógica de inicialización específica cuando el componente se adjunta.
-        console.log('El componente log3 se ha adjuntado a la entidad.');
-      },
+      // Aquí estamos creando algo como un "recordatorio" que podemos usar más adelante.
+      // En lugar de recordar la palabra `this` dentro de la función, la guardamos en una variable llamada `self`.
+      var self = this;
   
+      // En este paso, creamos una función especial que solo conocemos nosotros.
+      // Guardamos esta función en `this.eventHandlerFn` para que podamos referenciarla más adelante.
+      this.eventHandlerFn = function () {
+        // Cuando ocurra algo (como un evento), hacemos algo específico.
+        // En este caso, simplemente mostramos un mensaje en la consola que viene de `self.data.message`.
+        console.log(self.data.message);
+      };
+    },
+  
+    // Este método se llama cada vez que algo en nuestro componente cambia.
     update: function () {
-      var data = this.data;  // Almacena los valores de las propiedades del componente en la variable data para un fácil acceso.
-      var el = this.el;  // Guarda una referencia a la entidad a la que se ha adjuntado el componente.
+      // Almacenamos en variables locales algunos valores importantes que usaremos a continuación.
+      var data = this.data;  // Contiene los valores actuales de las propiedades del componente.
+      var el = this.el;      // Hace referencia a la entidad a la que se adjuntó este componente.
   
-        //Verifica si la propiedad event tiene un valor
+      // Verificamos si la propiedad `event` tiene un valor.
       if (data.event) {
-        // añade un escuchador de eventos a la entidad.Esto registra el `message` solo cuando la entidad emite el `event`.
-        el.addEventListener(data.event, function () {
-          console.log(data.message);
-        });
+        // Si `event` tiene un valor, añadimos un escuchador de eventos a la entidad.
+        // Cuando ocurra ese evento, ejecutaremos la función `this.eventHandlerFn`.
+        el.addEventListener(data.event, this.eventHandlerFn);
       } else {
-        // `event` not specified, just log the message.
+        // Si `event` no está especificado, simplemente mostramos el mensaje en la consola.
         console.log(data.message);
       }
     }
--- Registrar componente/app1.js
+++ Registrar componente/app1.js
@@ -4,7 +4,15 @@ window.addEventListener('load', function () {
   
     // Cambia dinámicamente la propiedad 'event' del componente 'log3' en la esfera
     sphereEntity.setAttribute('log3', 'event', 'anotherEvent');
+
+    
   });
+
+  AFRAME.registerComponent('hello-world', {
+    init: function () {
+        console.log('Hello, World!1');
+    }
+});
   
 AFRAME.registerComponent('log3', {
   schema: {
@@ -12,24 +20,36 @@ AFRAME.registerComponent('log3', {
     message: {type: 'string', default: 'Hello, World!3'}
   },
 
+  // Este es el lugar donde inicializamos nuestro componente cuando se adjunta.
   init: function () {
-      // Este es el lugar para la lógica de inicialización específica cuando el componente se adjunta.
-      console.log('El componente log3 se ha adjuntado a la entidad.');
-    },
+    // En lugar de recordar la palabra `this` dentro de la función, la guardamos en una variable llamada `self`.
+    var self = this;
 
+    // Guardamos esta función en `this.eventHandlerFn` para que podamos referenciarla más adelante.
+    this.eventHandlerFn = function () {
+      // Cuando ocurra algo (como un evento), hacemos algo específico.
+    
+      console.log(self.data.message);
+    };
+    console.log('punto1');
+  },
+
+  // Este método se llama cada vez que algo en nuestro componente cambia.
   update: function () {
-    var data = this.data;  // Almacena los valores de las propiedades del componente en la variable data para un fácil acceso.
-    var el = this.el;  // Guarda una referencia a la entidad a la que se ha adjuntado el componente.
-
-      //Verifica si la propiedad event tiene un valor
+    // Almacenamos en variables locales algunos valores importantes que usaremos a continuación.
+    var data = this.data;  // Contiene los valores actuales de las propiedades del componente.
+    var el = this.el;      // Hace referencia a la entidad a la que se adjuntó este componente.
+    console.log('punto2');
+    // Verificamos si la propiedad `event` tiene un valor.
     if (data.event) {
-      // añade un escuchador de eventos a la entidad.Esto registra el `message` solo cuando la entidad emite el `event`.
-      el.addEventListener(data.event, function () {
-        console.log(data.message);
-      });
+      // Si `event` tiene un valor, añadimos un escuchador de eventos a la entidad.
+      // Cuando ocurra ese evento, ejecutaremos la función `this.eventHandlerFn`.
+      el.addEventListener(data.event, this.eventHandlerFn);
+      console.log("punto3")
     } else {
-      // `event` not specified, just log the message.
+      // Si `event` no está especificado, simplemente mostramos el mensaje en la consola.
       console.log(data.message);
+      console.log("punto4")
     }
   }
 });
